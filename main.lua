273391ea5b0f36777c34a34ef61399198db0cf0e
antonioN313 <47403160+antonioN313@users.noreply.github.com>
2024-06-09 00:27:22 -0300

    update: match-0.5

---
+++ main.lua
@@ -0,0 +1,75 @@
+require 'src/Dependencies.lua'
+
+love.graphics.setDefaultFilter('nearest', 'nearest')
+
+function love.load()
+
+    love.window.setTitle('Match 3')
+
+    math.randomseed(os.time())
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        vsync = true,
+        fullscreen = false,
+        resizable = true,
+        canvas = true
+    })
+
+    gSounds['music']:setLooping(true)
+    gSounds['music']:play()
+
+    gStateMachine = StateMachine {
+        ['start'] = function() return StartState() end,
+        ['begin-game'] = function() return BeginGameState() end,
+        ['play'] = function() return PlayState() end,
+        ['game-over'] = function() return GameOverState() end,
+        ['victory'] = function() return VictoryState() end,
+        ['high-scores'] = function() return HighScoreState() end,
+        ['enter-high-score'] = function() return EnterHighScoreState() end,
+    }
+    gStateMachine:change('start',{
+        highScores = loadHighScores()
+    })
+
+    backgroundX = 0
+
+    love.keyboard.keysPressed = {}
+end
+
+function love.resize(w, h)
+    push:resize(w, h)
+end
+
+function love.keypressed(key)
+    love.keyboard.keysPressed[key] = true
+end
+
+function love.keyboard.wasPressed(key)
+    if love.keyboard.keysPressed[key] then
+        return true
+    else
+        return false
+    end
+end
+
+function love.update(dt)
+    
+    backgroundX = backgroundX - BACKGROUND_SCROLL_SPEED * dt
+    
+    if backgroundX <= -1024 + VIRTUAL_WIDTH - 4 + 51 then
+        backgroundX = 0
+    end
+
+    gStateMachine:update(dt)
+
+    love.keyboard.keysPressed = {}
+end
+
+function love.draw()
+    push:start()
+
+    love.graphics.draw(gTextures['background'], backgroundX, 0)
+    
+    gStateMachine:render()
+    push:finish()
+end
